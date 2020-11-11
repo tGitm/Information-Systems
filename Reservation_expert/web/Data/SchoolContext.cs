7c0b8deaa9dbac85d672cf601f3bddd7254a4495
tGitm <timrus99@gmail.com>
2020-11-11 12:26:06 +0100

    Reservation Expert

---
+++ Reservation_expert/web/Data/SchoolContext.cs
@@ -0,0 +1,26 @@
+using web.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace web.Data
+{
+    public class SchoolContext : IdentityDbContext<ApplicationUser>
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
